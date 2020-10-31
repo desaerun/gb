2d24548bc5db7f400b24f08fb5719e23ebe613a7
Charles <Charles.Cheney@retailerwebservices.com>
2020-10-31 00:43:23 -0700

    testing async.then for preference status

--- commands/general/is-birk-online.js
+++ commands/general/is-birk-online.js
@@ -1,12 +1,14 @@
 module.exports = {
     name: 'is-birk-online',
     description: "Reports on Birk's status",
-    async execute(client, message, args) {
+    execute(client, message, args) {
         try {
             const birkID = '97388794692505600';
-            const birk = await message.guild.members.fetch(birkID);
-        
-            await message.channel.send(`Kajoonie is ${birk.presence.status}.`);
+            let birk = async () => {
+                await message.guild.members.fetch(birkID);
+            };
+
+            birk.then((value) => message.channel.send(`Kajoonie is ${value.presence.status}.`));
         } catch(err) {
             console.error(err);
         }
