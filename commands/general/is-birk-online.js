3151378cf149df9835f789e6bc0667d87369128b
Charles <Charles.T.Cheney@gmail.com>
2020-10-30 21:12:39 -0700

    Update is-birk-online.js

--- commands/general/is-birk-online.js
+++ commands/general/is-birk-online.js
@@ -2,10 +2,14 @@ module.exports = {
     name: 'is-birk-online',
     description: "Reports on Birk's status",
     execute(client, message, args) {
-        const birkID = '97542223641464832';
-        const birk = await client.fetchUser(birkID);
+        try {
+            const birkID = '97542223641464832';
+            const birk = await client.fetchUser(birkID);
         
-        await message.channel.send(`Birk is ${birk.presence.status}.`);
+            await message.channel.send(`Birk is ${birk.presence.status}.`);
+        } catch(err) {
+            console.error(err);
+        }
     }
 }
         
