8ab30bcd4f932df4dcaab4c3af014988c10c6292
Charles <Charles.Cheney@retailerwebservices.com>
2020-10-31 02:29:55 -0700

    custom birk messages

--- commands/general/is-birk-online.js
+++ commands/general/is-birk-online.js
@@ -6,7 +6,26 @@ module.exports = {
             const birkID = '97542223641464832';
             let birk = await message.guild.members.fetch(birkID);
 
-            message.channel.send(`${birk.nickname} is ${birk.presence.status}.`);
+            let message;
+
+            switch (birk.presence.status) {
+                case 'dnd':
+                    message = `AY YO ${birk.nickname} DON'T WANNA BE FUCKED WITH RIGHT NOW`;
+                    break;
+                case 'idle':
+                    message = `This motherfucker ${birk.nickname} thinks he can just STEP AWAY?`;
+                    break;
+                case 'online':
+                    message = `WTF? Why is ${birk.nickname} online?`;
+                    break;
+                case 'offline':
+                    message = `Ah yes, a ${birk.nickname} in his natural habitat. Offline.`;
+                    break;
+                default:
+                    message = `tbh i don't even know what's going on right now`;
+            }
+
+            message.channel.send(message);
         } catch (err) {
             console.error(err);
         }
