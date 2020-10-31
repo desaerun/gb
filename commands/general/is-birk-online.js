d6c8c7996dd33de18a343d77111a4086aee4943e
Charles <Charles.Cheney@retailerwebservices.com>
2020-10-31 00:46:17 -0700

    learning async slowly

--- commands/general/is-birk-online.js
+++ commands/general/is-birk-online.js
@@ -1,15 +1,15 @@
 module.exports = {
     name: 'is-birk-online',
     description: "Reports on Birk's status",
-    execute(client, message, args) {
+    execute: function (client, message, args) {
         try {
             const birkID = '97388794692505600';
             let birk = async () => {
-                await message.guild.members.fetch(birkID);
+                return await message.guild.members.fetch(birkID);
             };
 
             birk().then((value) => message.channel.send(`Kajoonie is ${value.presence.status}.`));
-        } catch(err) {
+        } catch (err) {
             console.error(err);
         }
     }
