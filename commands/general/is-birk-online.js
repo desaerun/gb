49d83656a61889be940be9370282d4ac3c3b4167
Charles <Charles.T.Cheney@gmail.com>
2020-10-30 21:16:23 -0700

    Update is-birk-online.js

--- commands/general/is-birk-online.js
+++ commands/general/is-birk-online.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'is-birk-online',
     description: "Reports on Birk's status",
-    execute(client, message, args) {
+    async execute(client, message, args) {
         try {
             const birkID = '97542223641464832';
             const birk = await client.users.fetch(birkID);
