ef86e9b77f340c442a56bbf4e3fefce6db695ce6
Alex Jarvis <desaerun@gmail.com>
2020-10-31 04:05:21 -0400

    fix get-user-info.js to have fallback

--- commands/discord_control/get-user-info.js
+++ commands/discord_control/get-user-info.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'get-user-info',
     description: "get info on a user",
     execute(client, message, args) {
-        let user = message.mentions.members.first();
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.member(message.author);
         message.channel.send(user);
     }
 }
