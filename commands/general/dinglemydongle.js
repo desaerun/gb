918a7e4b6ca6f75f2041eedf3ba2e3ef952375b0
Alex Jarvis <desaerun@gmail.com>
2020-10-31 03:31:27 -0400

    fix dinglemydongle.js

--- commands/general/dinglemydongle.js
+++ commands/general/dinglemydongle.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'dinglemydongle',
     description: "dingles your dongle",
     execute(client, message, args) {
-        message.channel.send(`**${client.user.bot.username}** dingles **${message.author.username}**'s dongle.`);
+        message.channel.send(`**${client.user.username}** dingles **${message.author.username}**'s dongle.`);
     }
 }
