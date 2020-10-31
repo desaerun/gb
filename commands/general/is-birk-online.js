c87a21c207601851f001d929fe12de8f092d6c3f
Charles <Charles.Cheney@retailerwebservices.com>
2020-10-31 00:49:56 -0700

    fifteenth time's the charm

--- commands/general/is-birk-online.js
+++ commands/general/is-birk-online.js
@@ -1,14 +1,13 @@
 module.exports = {
     name: 'is-birk-online',
     description: "Reports on Birk's status",
-    execute: function (client, message, args) {
+    execute: async function (client, message, args) {
         try {
             const birkID = '97388794692505600';
-            let birk = async () => {
-                return await message.guild.members.fetch(birkID);
-            };
+            let birk = await message.guild.members.fetch(birkID);
+            let status = await birk.presence.status;
 
-            birk().then((value) => message.channel.send(`Kajoonie is ${value.presence.status}.`));
+            message.channel.send(`Kajoonie is ${status}.`);
         } catch (err) {
             console.error(err);
         }
