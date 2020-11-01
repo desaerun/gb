ac2765743418a7f9db849f09cc3bf05d4bcce109
Alex Jarvis <desaerun@gmail.com>
2020-11-01 04:56:05 -0500

    execute to listen in listener .js files

--- listeners/test.js
+++ listeners/test.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'test',
     description: 'test listener',
-    execute(client, message) {
+    listen(client, message) {
         if (message.content.includes('test')) {
             message.channel.send('this is an test listener response');
             return true;
