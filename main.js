0bbc438c7a3ffc587d5a80ed1cf3afb290410a1d
Alex Jarvis <desaerun@gmail.com>
2020-11-01 04:21:41 -0500

    console.log within parseWithListeners

--- main.js
+++ main.js
@@ -157,7 +157,8 @@ function runCommands(message) {
 function parseWithListeners(message) {
     try {
         for (const listener of client.listenerSet) {
-            if (client.listenerSet.get(listener).execute(client, message)) return;
+            console.log(listener);
+            if (listener.execute(client, message)) return;
         }
     } catch (err) {
         console.log(err);
