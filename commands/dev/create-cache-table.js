4b2174e30d449d9b45eb74808e31818055739841
Alex Jarvis <desaerun@gmail.com>
2020-11-03 01:25:18 -0500

    minor fix

--- commands/dev/create-cache-table.js
+++ commands/dev/create-cache-table.js
@@ -2,7 +2,6 @@ module.exports = {
     name: 'create-cache-table',
     description: 'Creates a table for the messages cache',
     execute: function(client,message,args) {
-        if(message.user.isAdmin())
-        let query = "CREATE TABLE messages (id BIGINT,date VARCHAR(255),guild BIGINT,channel,author,content,)"
+        let query = "CREATE TABLE messages (id BIGINT,date VARCHAR(255),guild BIGINT,channel,author,content,)";
     }
 }
