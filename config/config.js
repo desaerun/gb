92ba7de2c7f1b02d9cd17a19de3a2af3ec7feece
Alex Jarvis <desaerun@gmail.com>
2020-11-03 01:52:13 -0500

    add db config to config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,11 @@
 module.exports = {
+    db: {
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT,
+        database: process.env.RDS_DB_NAME,
+    },
     prefix : '-',
     verbosity : 3,
     guild_rageaholics_id : '270271948527894541',
@@ -6,5 +13,6 @@ module.exports = {
     user_josh_id : '95693092430020608',
     user_charles_id : '97388794692505600',
     channel_code_shit_id : '674824072126922753',
-    channel_dev_id : '576960974825979935'
+    channel_dev_id : '576960974825979935',
+
 }
