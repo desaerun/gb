4937b15cd436e7dec1e3be94bb20e91db9c3f41b
Alex Jarvis <desaerun@gmail.com>
2020-11-02 23:39:47 -0500

    change dev_output to chain .addfields instead of sending separate messages

--- dev_output.js
+++ dev_output.js
@@ -14,38 +14,39 @@ module.exports = {
         let error_text = stackTrace.get().toString();
         console.log(error_text);
 
+        let formatted_msg = new Discord.MessageEmbed()
+            .setAuthor(`${this.client.user.username}`, `${this.client.user.displayAvatarURL()}`)
+            .setColor('#a90d0d')
+            .addFields({
+                name: 'Error Generated',
+                value: error_message
+            })
         //Discord.MessageEmbed body cannot be longer than 1024 chars
         for (let i=0;i<error_text.length;i+=1024) {
-            let formatted_msg = new Discord.MessageEmbed()
-                .setAuthor(`${this.client.user.username}`, `${this.client.user.displayAvatarURL()}`)
-                .setColor('#a90d0d')
-                .addFields({
-                    name: 'Error Generated',
-                    value: error_message
-                })
+            formatted_msg
                 .addFields({
                     name: 'Stack Trace',
-                    value: error_text.substr(i,i+1024)
+                    value: error_text.substr(i, i + 1024)
                 });
-            switch (typeof (output_channel_ids)) {
-                case "string": {
-                    let channel = this.client.channels.cache.get(output_channel_ids);
-                    console.log(`this.client.channels.get(${output_channel_ids}): ${channel}`);
-                    channel.send(formatted_msg);
+        }
+        switch (typeof (output_channel_ids)) {
+            case "string": {
+                let channel = this.client.channels.cache.get(output_channel_ids);
+                console.log(`this.client.channels.get(${output_channel_ids}): ${channel}`);
+                channel.send(formatted_msg);
+                break;
+            }
+            case "object": {
+                if (output_channel_ids.isArray()) {
+                    for (let channel_id of output_channel_ids) {
+                        let channel = this.client.channels.cache.get(channel_id);
+                        channel.send(formatted_msg);
+                    }
                     break;
                 }
-                case "object": {
-                    if (output_channel_ids.isArray()) {
-                        for (let channel_id of output_channel_ids) {
-                            let channel = this.client.channels.cache.get(channel_id);
-                            channel.send(formatted_msg);
-                        }
-                        break;
-                    }
-                }
-                default: {
-                    console.log('could not send stack trace to output channel. output_channels is not a string or array.')
-                }
+            }
+            default: {
+                console.log('could not send stack trace to output channel. output_channels is not a string or array.')
             }
         }
     },
