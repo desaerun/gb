18526796e782085ff36ce5ec7ea4fbcdbef9e64a
Charles <Charles.Cheney@retailerwebservices.com>
2020-11-02 17:51:07 -0700

    regex map test

--- config/config.js
+++ config/config.js
@@ -3,6 +3,8 @@ module.exports = {
     verbosity : 3,
     guild_rageaholics_id : '270271948527894541',
     user_desaerun_id : '187048556643876864',
+    user_josh_id : '95693092430020608',
+    user_charles_id : '97388794692505600',
     channel_code_shit_id : '674824072126922753',
     channel_dev_id : '576960974825979935'
 }
--- listeners/josh-gym-shower-eat.js
+++ listeners/josh-gym-shower-eat.js
@@ -2,12 +2,19 @@ const CONFIG = require('../config/config');
 module.exports = {
     name: 'josh-gym-shower-eat',
     description: 'Responds to Josh talking about the gym, showering, or eating.',
+    wordResponseMap: this.buildWordResponseMap(),
     listen(client, message) {
-        const joshID = '95693092430020608';
-        const desID = '187048556643876864';
 
-        if (message.author.id !== joshID && message.author.id !== desID) return false;
+        if (message.author.id !== CONFIG.user_josh_id && message.author.id !== CONFIG.user_desaerun_id) return false;
 
+        for (let key in this.wordResponseMap.keys()) {
+            if (message.content.match(key)) {
+                let response = this.wordResponseMap.get(key);
+                message.channel.send(response);
+                return true;
+            }
+        }
+        /*
         // let messageContent = message.content.split(' ');
         const messageContent = message.content;
         let response;
@@ -26,7 +33,21 @@ module.exports = {
             message.channel.send(response);
             return true;
         }
+        */
 
         return false;
+    },
+
+    buildWordResponseMap() {
+        let map = new Map();
+
+        map.set(/\bgym\b/i, "Oh look Josh talking about the gym again.");
+        map.set(/\bshower\b/i, "Well hurry up Josh, I don't have all day.");
+        map.set(/\b(eat|food)\b/i, "Josh, you're going to get fat.");
+        map.set(/\bbail\b/i, "But Josh we neeeeeeeed you");
+        map.set(/\bchipotle\b/i, "I bet you could swallow one of those burritos whole.");
+        map.set(/\bno\b/i, "Yes.");
+
+        return map;
     }
 }
