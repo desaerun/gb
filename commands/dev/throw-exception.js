a77e1b853086421f0064ebadccf35bc31a6cd5f4
Alex Jarvis <desaerun@gmail.com>
2020-11-02 23:58:28 -0500

    add properties to throw-exception.js

--- commands/dev/throw-exception.js
+++ commands/dev/throw-exception.js
@@ -1,4 +1,6 @@
 module.exports = {
+    name: "throw-exception",
+    description: "Throws a text exception",
     execute: function (message, client) {
         throw new Error("This is a test error");
     }
