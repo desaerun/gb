c9ed101fc926a99bfa35d67ca3674cf962614fd0
Alex Jarvis <desaerun@gmail.com>
2020-11-04 19:31:45 -0500

    modify set-bot-status.js so it actually works

--- commands/bot_control/set-bot-status.js
+++ commands/bot_control/set-bot-status.js
@@ -2,7 +2,8 @@ module.exports = {
     name: 'set-bot-status',
     description: "sets the bot status",
     execute(client, message, args) {
-        client.user.setActivity('with discord.js',{type: 'PLAYING'})
+        let arg_string = args.join(" ");
+        client.user.setActivity(arg_string,{type: 'PLAYING'})
             .then(console.log())
             .catch(console.error);
     }
