2e79d7234af7d31f1c2805066a071d87ac6fd92f
Alex Jarvis <desaerun@gmail.com>
2020-11-12 02:40:40 -0500

    fix directory names, can't use ~ from inside node

--- commands/dev/print-log.js
+++ commands/dev/print-log.js
@@ -9,19 +9,19 @@ module.exports = {
             }
             message.channel.send(`Contents of bot log file: \`\`\`${data}\`\`\``);
         })
-        fs.readFile('~/.pm2/logs/groidbot-out.log','utf8',(err,data) => {
+        fs.readFile('/home/groidbot/.pm2/logs/groidbot-out.log','utf8',(err,data) => {
             if (err) {
                 return console.log(err)
             }
             message.channel.send(`Contents of pm2 stdout log file: \`\`\`${data}\`\`\``);
         })
-        fs.readFile('~/.pm2/logs/groidbot-error.log','utf8',(err,data) => {
+        fs.readFile('/home/groidbot/.pm2/logs/groidbot-error.log','utf8',(err,data) => {
             if (err) {
                 return console.log(err)
             }
             message.channel.send(`Contents of pm2 error log file: \`\`\`${data}\`\`\``);
         })
-        fs.readFile('~/.pm2/logs/groidbot-error.log','utf8',(err,data) => {
+        fs.readFile('/home/groidbot/.pm2/logs/groidbot-error.log','utf8',(err,data) => {
             if (err) {
                 return console.log(err)
             }
