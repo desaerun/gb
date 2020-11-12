e3f47c39af6fc64e41ac121923bd339523069889
Alex Jarvis <desaerun@gmail.com>
2020-11-12 02:18:04 -0500

    print-logs command

--- commands/dev/print-log.js
+++ commands/dev/print-log.js
@@ -1,7 +1,13 @@
+fs = require('fs');
 module.exports = {
     name: "print-log",
     description: "prints the logfile",
     execute: function (message, client) {
-        throw new Error("This is a test error");
+        fs.readFile('/var/log/groidbot.log','utf8',(err,data) => {
+            if (err) {
+                return console.log(err)
+            }
+            message.channel.send(`Contents of log file: \`\`\`${data}\`\`\``);
+        })
     }
 }
