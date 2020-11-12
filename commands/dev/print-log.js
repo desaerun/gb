cf04acb4d4379242aa3c969d610c2055daad8c12
Alex Jarvis <desaerun@gmail.com>
2020-11-12 02:39:26 -0500

    print more logs

--- commands/dev/print-log.js
+++ commands/dev/print-log.js
@@ -7,7 +7,25 @@ module.exports = {
             if (err) {
                 return console.log(err)
             }
-            message.channel.send(`Contents of log file: \`\`\`${data}\`\`\``);
+            message.channel.send(`Contents of bot log file: \`\`\`${data}\`\`\``);
+        })
+        fs.readFile('~/.pm2/logs/groidbot-out.log','utf8',(err,data) => {
+            if (err) {
+                return console.log(err)
+            }
+            message.channel.send(`Contents of pm2 stdout log file: \`\`\`${data}\`\`\``);
+        })
+        fs.readFile('~/.pm2/logs/groidbot-error.log','utf8',(err,data) => {
+            if (err) {
+                return console.log(err)
+            }
+            message.channel.send(`Contents of pm2 error log file: \`\`\`${data}\`\`\``);
+        })
+        fs.readFile('~/.pm2/logs/groidbot-error.log','utf8',(err,data) => {
+            if (err) {
+                return console.log(err)
+            }
+            message.channel.send(`Contents of pm2 status log file: \`\`\`${data}\`\`\``);
         })
     }
 }
