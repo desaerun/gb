c735dd9c98976cd9296a4fd3159169ac49f2b21d
Alex Jarvis <desaerun@gmail.com>
2020-11-12 11:40:59 -0500

    logfile update to read first 15 lines only

--- commands/dev/print-log.js
+++ commands/dev/print-log.js
@@ -2,31 +2,27 @@ fs = require('fs');
 module.exports = {
     name: "print-log",
     description: "prints the logfile",
-    //todo: this errors out because logfile >2000 chars?
     execute: function (client, message, args) {
-        fs.readFile('/var/log/groidbot.log','utf8',(err,data) => {
-            if (err) {
-                return console.log(err)
-            }
-            message.channel.send(`Contents of bot log file: \`\`\`${data.substr(0,1500)}\`\`\``);
-        })
-        fs.readFile('/home/groidbot/.pm2/logs/groidbot-out.log','utf8',(err,data) => {
-            if (err) {
-                return console.log(err)
-            }
-            message.channel.send(`Contents of pm2 stdout log file: \`\`\`${data.substr(0,1500)}\`\`\``);
-        })
-        fs.readFile('/home/groidbot/.pm2/logs/groidbot-error.log','utf8',(err,data) => {
-            if (err) {
-                return console.log(err)
-            }
-            message.channel.send(`Contents of pm2 error log file: \`\`\`${data.substr(0,1500)}\`\`\``);
-        })
-        fs.readFile('/home/groidbot/.pm2/logs/groidbot-error.log','utf8',(err,data) => {
-            if (err) {
-                return console.log(err)
-            }
-            message.channel.send(`Contents of pm2 status log file: \`\`\`${data.substr(0,1500)}\`\`\``);
-        })
+        message.channel.send(`Contents of bot log file: \`\`\`${readLog('/var/log/groidbot.log')}\`\`\``);
+        message.channel.send(`Contents of pm2 status log file:: \`\`\`${readLog('/home/groidbot/.pm2/pm2.log')}\`\`\``);
+        message.channel.send(`Contents of pm2 stdout log file:: \`\`\`${readLog('/home/groidbot/.pm2/logs/groidbot-out.log')}\`\`\``);
+        message.channel.send(`Contents of pm2 error log file:: \`\`\`${readLog('/home/groidbot/.pm2/logs/groidbot-error.log')}\`\`\``);
     }
+}
+function readLog(file,num_lines = 15) {
+    let lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(file),
+    });
+    let lineCounter = 0; let lines = [];
+    lineReader.on('line',(line) => {
+        lineCounter++;
+        lines.push(line);
+        if(lineCounter === num_lines) {
+            lineReader.close();
+        }
+    });
+    lineReader.on('close',() => {
+        return(lines.join("\n"));
+    })
+    return false;
 }
