919734463e594b794b28db260f80d51a26d41af5
Alex Jarvis <desaerun@gmail.com>
2020-11-12 02:13:10 -0500

    move log from /var/log to project dir

---
+++ commands/dev/print-log.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "print-log",
+    description: "prints the logfile",
+    execute: function (message, client) {
+        throw new Error("This is a test error");
+    }
+}
