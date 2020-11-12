263e18d192044c4d21ce8f1b7a69c104dc024957
Alex Jarvis <desaerun@gmail.com>
2020-11-12 02:34:58 -0500

    fix function call parameters in print-log.js and throw-exception.js

--- commands/dev/print-log.js
+++ commands/dev/print-log.js
@@ -2,7 +2,7 @@ fs = require('fs');
 module.exports = {
     name: "print-log",
     description: "prints the logfile",
-    execute: function (message, client) {
+    execute: function (client, message, args) {
         fs.readFile('/var/log/groidbot.log','utf8',(err,data) => {
             if (err) {
                 return console.log(err)
