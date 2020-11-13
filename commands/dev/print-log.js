026d7faf3739970c922121c181c9cc3d32ccbe0a
Alex Jarvis <desaerun@gmail.com>
2020-11-13 13:15:48 -0500

    async log file

--- commands/dev/print-log.js
+++ commands/dev/print-log.js
@@ -2,11 +2,11 @@ fs = require('fs');
 module.exports = {
     name: "print-log",
     description: "prints the logfile",
-    execute: function (client, message, args) {
-        message.channel.send(`Contents of bot log file: \`\`\`${readLog('/var/log/groidbot.log')}\`\`\``);
-        message.channel.send(`Contents of pm2 status log file: \`\`\`${readLog('/home/groidbot/.pm2/pm2.log')}\`\`\``);
-        message.channel.send(`Contents of pm2 stdout log file: \`\`\`${readLog('/home/groidbot/.pm2/logs/groidbot-out.log')}\`\`\``);
-        message.channel.send(`Contents of pm2 error log file: \`\`\`${readLog('/home/groidbot/.pm2/logs/groidbot-error.log')}\`\`\``);
+    execute: async function (client, message, args) {
+        message.channel.send(`Contents of bot log file: \`\`\`${await readLog('/var/log/groidbot.log')}\`\`\``);
+        message.channel.send(`Contents of pm2 status log file: \`\`\`${await readLog('/home/groidbot/.pm2/pm2.log')}\`\`\``);
+        message.channel.send(`Contents of pm2 stdout log file: \`\`\`${await readLog('/home/groidbot/.pm2/logs/groidbot-out.log')}\`\`\``);
+        message.channel.send(`Contents of pm2 error log file: \`\`\`${await readLog('/home/groidbot/.pm2/logs/groidbot-error.log')}\`\`\``);
     }
 }
 async function readLog(file,num_lines = 15) {
