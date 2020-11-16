98de269230dbf4c4e455f10dc7bdbaa3c9da1265
Alex Jarvis <desaerun@gmail.com>
2020-11-16 02:28:49 -0500

    update discord token

--- commands/dev/print-env.js
+++ commands/dev/print-env.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "prints environment variables",
     execute: function (client,message,args) {
         message.channel.send(`
-            \`\`\`DISCORD_KEY=${process.env.DISCORD_TOKEN}\`\`\`
+            \`\`\`DISCORD_KEY=${process.env.BOT_TOKEN}\`\`\`
             \`\`\`YOUTUBE_KEY=${process.env.YOUTUBE_TOKEN}\`\`\`
         `)
     }
