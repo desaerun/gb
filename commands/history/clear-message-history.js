be14f7eb344b94b0e8fa8cbeb4acd0c87b9da3d1
Alex Jarvis <desaerun@gmail.com>
2020-11-17 03:51:53 -0500

    add callback function to mysql query in clear-message-history.js

--- commands/history/clear-message-history.js
+++ commands/history/clear-message-history.js
@@ -3,6 +3,12 @@ module.exports = {
     name: 'clear-message-history',
     description: "Clears all message history from db",
     execute: async function (client, message, args) {
-        await mysqlQuery.sqlConnection("TRUNCATE TABLE `messages`");
+        await mysqlQuery.sqlConnection("TRUNCATE TABLE `messages`",(err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                message.reply("Successfully deleted message history");
+            }
+        });
     }
 }
