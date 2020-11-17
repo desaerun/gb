e1e81e6b78b803f1c5154220f09077481dfe88d5
Alex Jarvis <desaerun@gmail.com>
2020-11-17 03:49:32 -0500

    async mysql function in clear-message-history.js

--- commands/history/clear-message-history.js
+++ commands/history/clear-message-history.js
@@ -3,6 +3,6 @@ module.exports = {
     name: 'clear-message-history',
     description: "Clears all message history from db",
     execute: async function (client, message, args) {
-        mysqlQuery("TRUNCATE TABLE `messages`",);
+        await mysqlQuery.sqlConnection("TRUNCATE TABLE `messages`");
     }
 }
