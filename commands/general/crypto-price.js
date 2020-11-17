1c785d03023ac7fcc30c24afe203d9859381fd53
Charles <Charles.Cheney@retailerwebservices.com>
2020-11-17 16:39:41 -0700

    don't mind me just playing with coinbase API

---
+++ commands/general/crypto-price.js
@@ -0,0 +1,29 @@
+const Client = require('coinbase').Client;
+
+module.exports = {
+    name: 'coinbase',
+    description: "Retrieves crypto prices from Coinbase API (in USD)",
+    execute(client, message, args) {
+        if (!args) {
+            message.channel.send('You must include a crypto ticker (BTC, ETH) with this request.');
+            return;
+        }
+
+        if (args.length > 1) {
+            message.channel.send(`You cannot make multiple requests at once. (Requested ${args})`);
+            return;
+        }
+
+        let currency = args[0];
+        let coinbaseClient = new Client();
+
+        coinbaseClient.getExchangeRates({'currency': currency}, (err, rates) => {
+            if (err) {
+                message.channel.send(err);
+            } else {
+                let response = JSON.parse(rates);
+                message.channel.send(`1 ${currency} = \$${response.data.rates.USD}`);
+            }
+        });
+    }
+}
