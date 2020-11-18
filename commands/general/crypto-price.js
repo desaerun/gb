946f2851dfd7f314b9b3c87200e9c17c6fc76020
Charles <Charles.Cheney@retailerwebservices.com>
2020-11-17 17:49:07 -0700

    testing coinbase pro stats endpoint

--- commands/general/crypto-price.js
+++ commands/general/crypto-price.js
@@ -1,10 +1,19 @@
 const request = require('request');
 
+const currencyFormat = new Intl.NumberFormat('en-US',
+    {style: 'currency',
+            currency: 'USD' });
+
+const percentFormat = new Intl.NumberFormat('en-US',
+    {style: 'percent',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2})
+
 module.exports = {
     name: 'crypto-price',
     description: "Retrieves crypto prices from Coinbase API (in USD)",
     execute(client, message, args) {
-        if (!args) {
+        if (args.length < 1) {
             message.channel.send('You must include a crypto ticker (BTC, ETH) with this request.');
             return;
         }
@@ -16,12 +25,18 @@ module.exports = {
 
         let crypto = args[0];
 
-       request(`https://api.coinbase.com/v2/exchange-rates?currency=${crypto}`, function (err, response, body) {
+       request(`https://api.pro.coinbase.com/products/${crypto}-USD/stats`, function (err, response, body) {
           if (!err && response.statusCode == 200) {
               let coinbaseData = JSON.parse(body);
-              let usdVal = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD' })
-                  .format(coinbaseData.data.rates.USD);
-              message.channel.send(`1 ${crypto} = ${usdVal}`);
+
+              let priceDiff = coinbaseData.last - coinbaseData.open;
+              let percDiff = (priceDiff / coinbaseData.open) * 100;
+
+              let curPriceFormatted = currencyFormat.format(coinbaseData.last);
+              let priceDiffFormatted = currencyFormat.format(priceDiff);
+              let percDiffFormatted = (priceDiff > 0 ? '' : '-') + percentFormat.format(percDiff);
+
+              message.channel.send(`1 ${crypto} = ${curPriceFormatted} (${priceDiffFormatted} / ${percDiffFormatted}`);
           } else {
               message.channel.send(err);
           }
