e1d046bbe510129ae88d351006a944385df797da
Alex Jarvis <desaerun@gmail.com>
2020-11-23 16:40:58 -0500

    is there more data in cached guild info?

--- listeners/capture-message.js
+++ listeners/capture-message.js
@@ -21,7 +21,7 @@ module.exports = {
         let author_values = {
             id: message.author.id,
             guild: guild_values.id,
-            displayName: message.author.displayName,
+            displayName: message.author.username,
         }
         let message_values = {
             id: message.id,
@@ -31,10 +31,11 @@ module.exports = {
             content: message.content,
             timestamp: snowflakeToTimestamp(message.id),
         }
-        console.log(JSON.stringify(message.guild) + "..." + JSON.stringify(guild_values));
-        console.log(JSON.stringify(message.channel) + "..." + JSON.stringify(channel_values));
-        console.log(JSON.stringify(message.author) + "..." + JSON.stringify(author_values));
-        console.log(JSON.stringify(message) + "..." + JSON.stringify(message_values));
+        console.log("Guild: " + JSON.stringify(message.guild) + "..." + JSON.stringify(guild_values));
+        console.log("Channel: " +JSON.stringify(message.channel) + "..." + JSON.stringify(channel_values));
+        console.log("Author: " +JSON.stringify(message.author) + "..." + JSON.stringify(author_values));
+        console.log("Cached Author:" + JSON.stringify(message.guild.users.cache.get(author_values.id)));
+        console.log("Message: " +JSON.stringify(message) + "..." + JSON.stringify(message_values));
         conn.query("INSERT INTO guilds SET ? ON DUPLICATE KEY UPDATE ?", [guild_values, guild_values], (error,result,fields) => {
             if (error) throw error;
             console.log("Successfully inserted Guild");
