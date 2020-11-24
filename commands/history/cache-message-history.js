b82285f34575bb052176cb48e4fb5cfe358936d3
Alex Jarvis <desaerun@gmail.com>
2020-11-24 02:57:13 -0500

    modify cache-message-history.js to use capture-message.js

--- commands/history/cache-message-history.js
+++ commands/history/cache-message-history.js
@@ -12,29 +12,10 @@ module.exports = {
     description: "Retrieves message history for the current channel and stores it to the DB",
     execute: async function (client, message, args) {
 
-        let guild = message.guild;
-        let channel = message.channel;
-        let guild_values = {
-            id: message.guild.id,
-            name: message.guild.name,
-        }
-        let channel_values = {
-            id: channel.id,
-            guild: guild_values.id,
-            name: channel.name,
-        }
-        conn.query(`INSERT INTO guilds SET ? ON DUPLICATE KEY UPDATE ?`, [guild_values, guild_values], (error, result, fields) => {
-            if (error) throw error;
-            console.log("successfully inserted guild");
-        });
-        conn.query(`INSERT INTO channels SET ? ON DUPLICATE KEY UPDATE ?`, [channel_values, channel_values], (error, result, fields) => {
-            if (error) throw error;
-            console.log("successfully inserted channel");
-        });
         let messageCount = 0;
         console.log(`Retrieving list of messages...`);
 
-        let messages = await channel.messages.fetch({limit: 100});
+        let messages = await message.channel.messages.fetch({limit: 100});
 
         while (messages.size > 0) {
             messageCount += messages.size;
@@ -42,50 +23,9 @@ module.exports = {
 
             for (let historical_message of messages.values()) {
                 captureMessage(historical_message);
-                /*let message_timestamp = snowflakeToTimestamp(historical_message.id);
-
-                //insert into DB for author
-                let author = client.guilds.cache.get(guild_values.id).member(historical_message.author.id);
-                let author_displayName = author ? author.displayName : "NULL";
-                let author_values = {
-                    id: historical_message.author.id,
-                    displayName: author_displayName,
-                }
-                conn.query(`INSERT INTO users SET ? ON DUPLICATE KEY UPDATE ?`, [author_values, author_values], (error, results, fields) => {
-                    if (error) {
-                        console.log("mysql insert of user failed");
-                        throw error;
-                    }
-                    console.log(`inserted user ${author_values.displayName}(${author_values.id}) successfully`);
-                });
-
-                //insert into DB for message
-                let post = {
-                    id: historical_message.id,
-                    author: historical_message.author.id,
-                    guild: guild.id,
-                    channel: channel.id,
-                    content: historical_message.content,
-                    timestamp: message_timestamp,
-                }
-
-                //debug
-
-
-                conn.query(`INSERT INTO messages SET ? ON DUPLICATE KEY UPDATE ?`, [post, post], (error, results, fields) => {
-                    if (error) {
-                        console.log("mysql insert of message failed");
-                        throw error;
-                    }
-                    console.log(`Adding message to db: ${post.id}`);
-                    console.log(`Message Timestamp: ${moment.utc(post.timestamp).format("MMMM Do YYYY, h:mm:ss a")}`);
-                    console.log(`Guild ID: ${guild_values.id}`);
-                    console.log(`Author ID: ${author_values.id}`);
-                    console.log(`Author Display Name: ${author_values.displayName}`);
-                    console.log(`inserted message ${post.id} successfully`);
-                });*/
             }
-            messages = await channel.messages.fetch({limit: 100, before: last});
+            messages = await message.channel.messages.fetch({limit: 100, before: last});
+            console.log(`End of batch, messages.size=${messages.size}`);
         }
 
         messageCount += messages.size;
