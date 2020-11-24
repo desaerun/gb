649ae0ec716d1ac40ce82b34ec5154a10c6659f7
Alex Jarvis <desaerun@gmail.com>
2020-11-24 02:43:26 -0500

    make description more verbose, ensure other listeners are executed

--- listeners/capture-message.js
+++ listeners/capture-message.js
@@ -1,8 +1,9 @@
 captureMessage = require("../tools/capture-message");
 module.exports = {
     name: "capture-message",
-    description: "Captures a message",
+    description: "Listens for a message, captures and stores it to the db",
     listen(client, message) {
         captureMessage(message);
+        return false; //make sure other listeners are executed
     }
 }
