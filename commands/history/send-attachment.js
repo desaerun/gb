957e6e65dd24042c3a8548c42cc7e67414a8fb88
Alex Jarvis <desaerun@gmail.com>
2020-11-26 21:11:42 -0500

    modify send_attachment

--- commands/history/send-attachment.js
+++ commands/history/send-attachment.js
@@ -1,8 +1,9 @@
-const { MessageAttachment } = require("discord.js");
+const {MessageAttachment} = require("discord.js");
 const locutus = require("locutus");
 const mysql = require('mysql');
 const db = require("../../config/db");
 const conn = mysql.createConnection(db);
+const Discord = require("discord.js");
 conn.connect();
 
 module.exports = {
@@ -20,23 +21,35 @@ module.exports = {
                     let attachment = new MessageAttachment(attachmentURL);
                     await message.channel.send(`Test attachment`, attachment);
                 }
-            })
-        }
-        else {
+            });
+        } else {
             let arg_str = args.join(" ");
             let timestamp = locutus.php.datetime.strtotime(arg_str) * 1000; //convert string to timestamp
             console.log(`Locutus timestamp: ${timestamp}`);
             timestamp -= timestamp % (24 * 60 * 60 * 1000); //subtract minutes since midnight
-            let end_timestamp = timestamp + (24 *60 * 60 * 1000);
+            let end_timestamp = timestamp + (24 * 60 * 60 * 1000);
+            let timestamp_now = new Date.now();
             console.log(`${timestamp} :: ${end_timestamp}`);
-            conn.query("SELECT * FROM attachments WHERE timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC",[timestamp,end_timestamp], async (error, result, fields) => {
+            conn.query("SELECT `m`.`content`,`a`.`url` AS `attachmentURL` FROM `messages` `m` LEFT JOIN `attachments` `a` ON `m`.`id`=`a`.`message_id` WHERE `m`.`channel` = ? AND `m`.`timestamp` >= ? AND `m`.`timestamp` <= ? ORDER BY `m`.`timestamp` DESC", [message.channel.id, timestamp, end_timestamp], async (error, result, fields) => {
                 if (error) throw error;
-                console.log(result);
-                for (const attachmentRow of result) {
-                    const attachmentURL = attachmentRow.url;
-                    console.log(`Attachment URL: ${attachmentURL}`);
-                    let attachment = new MessageAttachment(attachmentURL);
-                    await message.channel.send(`Test attachment`, attachment);
+                for (const messageRow of result) {
+                    const attachmentURL = messageRow.url;
+                    conn.query("SELECT * FROM `users` WHERE `id` = ? LIMIT 1", messageRow.author, async (error, authors, fields) => {
+                        let author = authors[0];
+                        //let attachment = new MessageAttachment(attachmentURL);
+                        let embedMessage = new Discord.MessageEmbed()
+                            .setAuthor(author.displayName, author.avatarURL)
+                            .addField('', messageRow.content)
+                            .setTimestamp(messageRow.timestamp);
+                        if (attachmentURL) {
+                            embedMessage.setImage(attachmentURL);
+                        }
+                        try {
+                            await message.channel.send(embedMessage);
+                        } catch (err) {
+                            console.error("There was an error sending the embed message:", err);
+                        }
+                    });
                 }
             });
         }
