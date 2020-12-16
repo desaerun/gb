023558c3cafb63af056917772a8040423d1267b2
Alex Jarvis <desaerun@gmail.com>
2020-12-16 01:57:18 -0500

    allow arg for channel ID

--- commands/history/cache-message-history.js
+++ commands/history/cache-message-history.js
@@ -12,13 +12,19 @@ module.exports = {
     description: "Retrieves message history for the current channel and stores it to the DB",
     execute: async function (client, message, args) {
 
+        let targetChannel = message.channel;
+        //if command is called with arg, check if it's a channel ID;
         if (args.length === 0) {
-            //change target channel to ID passed in argument
+            if (message.channels.cache.get(args[0])) {
+                targetChannel = message.guild.channels.get(args[0]);
+            } else {
+                message.channel.send("The specified channel ID was not found.");
+                return false;
+            }
         }
-
         console.log(`Retrieving list of messages...`);
 
-        let messages = await message.channel.messages.fetch({limit: 100});
+        let messages = await targetChannel.messages.fetch({limit: 100});
         let messageCount = 0;
         while (messages.size > 0) {
             console.log(`*************Start of batch, messages.size=${messages.size}**************`);
@@ -28,14 +34,14 @@ module.exports = {
             for (let historical_message of messages.values()) {
                 captureMessage(client,historical_message);
             }
-            messages = await message.channel.messages.fetch({limit: 100, before: last});
+            messages = await targetChannel.messages.fetch({limit: 100, before: last});
             console.log(`*************End of batch, messages.size=${messages.size}*************`);
         }
         messageCount += messages.size;
 
-        message.reply(`There have been ${messageCount} messages sent in this channel.`);
-        conn.query(`SELECT COUNT(*) FROM messages WHERE channel_id = ${message.channel.id}`,(err,result,fields) => {
-            message.reply(`Updated mysql query successfully.  Rows: ${JSON.stringify(result)}`);
+        message.channel.send(`There have been ${messageCount} messages sent in this channel.`);
+        conn.query(`SELECT COUNT(*) FROM messages WHERE channel_id = ${targetChannel.id}`,(err,result,fields) => {
+            message.channel.send(`Updated mysql query successfully.  Rows: ${JSON.stringify(result)}`);
         });
     }
 }
