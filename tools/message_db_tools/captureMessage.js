de985d8fac444253194e26846fe8e8cef25bed46
Alex Jarvis <desaerun@gmail.com>
2020-12-21 17:17:07 -0500

    testing other async method

--- tools/message_db_tools/captureMessage.js
+++ tools/message_db_tools/captureMessage.js
@@ -10,7 +10,30 @@ conn.connect();
 
 
 captureMessage = async function (client,message,includeBotMessages = false) {
-    await conn.query("SELECT * FROM messages WHERE id = ?", message.id, (err, result, fields) => {
+    let e_result = await conn.query("SELECT * FROM messages WHERE id = ?", message.id);
+    if (e_result.length === 0) { // if message doesn't already exist in DB
+        const author = message.guild.members.cache.get(message.author.id);
+        if (!author) {
+            console.log(`Author was not able to be fetched for message ${message.id}`);
+            console.log(message);
+            return 4; // no author
+        } else {
+            //todo: save embedded messages
+            // console.log(`Author: ${JSON.stringify(author)}`);
+            if (!author.user.bot || includeBotMessages) {
+                insertNewMessage(message);
+                return 1; // added
+            } else {
+                console.log("Message was from a bot and includeBotMessages is false.");
+                return 3; // bot message
+            }
+        }
+    } else {
+        console.log(`Message ${message.id} already exists in DB, skipping...`);
+        return 2; // skipped
+    }
+    /*
+    await conn.query("SELECT * FROM messages WHERE id = ?", message.id, (err, result) => {
         if (err) throw err;
         if (result.length === 0) { // if message doesn't already exist in DB
             const author = message.guild.members.cache.get(message.author.id);
@@ -34,6 +57,8 @@ captureMessage = async function (client,message,includeBotMessages = false) {
             return 2; // skipped
         }
     });
+
+     */
 }
 
 module.exports = captureMessage;
