8c289d01a6977666b8ffa4f9fac539db19e85134
Alex Jarvis <desaerun@gmail.com>
2020-12-23 12:56:08 -0500

    utc offset

--- commands/history/random-message.js
+++ commands/history/random-message.js
@@ -48,10 +48,10 @@ module.exports = {
         let utcOffset = moment(timestamp).utcOffset();
         let offsetMs = -utcOffset * 60 * 1000;
         console.log(`utcOffset: ${utcOffset}|offsetMs: ${offsetMs}`);
-        timestamp += offsetMs;
 
         //calculate midnight on both ends of the day provided
         timestamp -= timestamp % (24 * 60 * 60 * 1000); //subtract minutes since midnight
+        timestamp += offsetMs; // add the UTC offset
         let end_timestamp = timestamp + (24 * 60 * 60 * 1000) - 1; //get 11:59:59.999 at the end of that day
         console.log(`Selecting messages between (${timestamp})${moment(timestamp).format('MMMM Do YYYY HH:mm:ss a')} and (${end_timestamp})${moment(end_timestamp).format('MMMM Do YYYY HH:mm:ss a')}`);
         console.log(`${timestamp} :: ${end_timestamp}`);
