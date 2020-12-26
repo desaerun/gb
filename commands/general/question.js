366f7c0d41bf322ddfdacdda414705d998200ea6
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-25 17:43:58 -0700

    gay stuff

---
+++ commands/general/question.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+module.exports = {
+    name: 'question',
+    description: 'Attempts to answer your question',
+    async execute(client, message, args) {
+        if (args.length < 1) {
+            message.channel.send('You gotta include a question, dummy.');
+            return;
+        }
+
+        let query = args.join('+');
+
+        let googleQueryURL = `https://www.google.com/search?q=${query}`;
+
+        try {
+            const response = await axios.get(googleQueryURL);
+            if (response.status === 200) {
+                let answerHTMLTag = 'data-tts-text="';
+                let startIndex = response.data.indexOf(answerHTMLTag) + answerHTMLTag.length + 1;
+
+                if (startIndex === -1) {
+                    message.channel.send('Unable to find an answer. Please go fuck yourself.');
+                    return;
+                }
+
+                let endIndex = response.data.indexOf('"', startIndex);
+
+                let answer = response.data.substring(startIndex, endIndex);
+
+                message.channel.send(answer);
+            } else {
+                throw new Error(`Request returned status code ${response.status}`);
+            }
+        } catch (err) {
+            message.channel.send(`Error encountered while attempting to answer your question: ${err}`);
+        }
+    }
+}
