f6d71ce0ed4b79efa3cd5f6170c8bbaeb720b5eb
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-26 18:19:51 -0700

    Some code cleanup

--- commands/general/question.js
+++ commands/general/question.js
@@ -22,46 +22,15 @@ module.exports = {
                 // Remove 'Videos' box from search results
                 cheerioDOM('div.HD8Pae.luh4tb.cUezCb.xpd.O9g5cc.uUPGi').remove();
 
-                let innerDOMString = cheerioDOM("div[aria-level='3']").first().html();
-                let answer;
-                if (innerDOMString) {
-                    const innerDOM = cheerio.load(innerDOMString);
-
-                    // Remove some of the subtext in featured results
-                    innerDOM('div.yxAsKe.kZ91ed').remove();
-                    innerDOM('span.kX21rb').remove();
-
-                    answer = innerDOM.text();
+                if (retrieveAnswerFromFeaturedSnippet(message, cheerioDOM)) {
+                    return;
                 }
 
-                if (answer) {
-                    let context = cheerioDOM('span.hgKElc').text();
-
-                    if (!context || answer === context) {
-                        message.channel.send(answer);
-                    } else {
-                        message.channel.send(`**${answer}**\n${context}`);
-                    }
-
-                } else {
-                    let complementaryResults = cheerioDOM("div[id='wp-tabs-container']").html();
-                    
-                    const innerDOM = cheerio.load(complementaryResults);
-
-                    answer = innerDOM("h2[data-attrid='title']").text();
-
-                    if (answer) {
-                        let context = innerDOM('div.kno-rdesc > div > span').first().text();
-                        if (!context || answer === context) {
-                            message.channel.send(answer);
-                        } else {
-                            message.channel.send(`**${answer}**\n${context}`);
-                        }
-
-                    } else {
-                        message.channel.send('Unable to find an answer. Please go fuck yourself.');
-                    }
+                if (retrieveAnswerFromKnowledgePanel(message, cheerioDOM)) {
+                    return;
                 }
+
+                message.channel.send('Unable to find an answer. Please go fuck yourself.');
 
             } else {
                 throw new Error(`Request returned status code ${response.status}`);
@@ -70,4 +39,73 @@ module.exports = {
             message.channel.send(`Error encountered while attempting to answer your question: ${err}`);
         }
     }
+}
+
+/**
+ * Attempts to retrieve Google Search results contained within the "Featured Snippet" above the
+ * actual search results to provide in answer. Returns false if a response is not sent to Discord,
+ * true otherwise.
+ *
+ * @param message
+ * @param cheerioDOM
+ * @returns {boolean}
+ */
+function retrieveAnswerFromFeaturedSnippet(message, cheerioDOM) {
+
+    // Grabbing the first aria-level 3 div should give us the Featured Snippet box if it exists
+    let innerDOMString = cheerioDOM("div[aria-level='3']").first().html();
+
+    if (innerDOMString) {
+        const innerDOM = cheerio.load(innerDOMString);
+
+        // Remove some of the subtext in featured snippet
+        innerDOM('div.yxAsKe.kZ91ed').remove();
+        innerDOM('span.kX21rb').remove();
+
+        let answer = innerDOM.text();
+
+        if (answer) {
+            let context = cheerioDOM('span.hgKElc').text();
+
+            if (!context || answer === context) {
+                message.channel.send(answer);
+                return true;
+            } else {
+                message.channel.send(`**${answer}**\n${context}`);
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Attempts to retrieve Google Search results contained within the "Knowledge Panel" on the right side
+ * of the results page, if it exists. Returns false if a response is not sent to Discord, true otherwise.
+ *
+ * @param message
+ * @param cheerioDOM
+ */
+function retrieveAnswerFromKnowledgePanel(message, cheerioDOM) {
+    let knowledgePanel = cheerioDOM("div[id='wp-tabs-container']").html();
+
+    if (knowledgePanel) {
+        const innerDOM = cheerio.load(knowledgePanel);
+
+        let answer = innerDOM("h2[data-attrid='title']").text();
+
+        if (answer) {
+            let context = innerDOM('div.kno-rdesc > div > span').first().text();
+            if (!context || answer === context) {
+                message.channel.send(answer);
+                return true;
+            } else {
+                message.channel.send(`**${answer}**\n${context}`);
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
