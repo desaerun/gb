1c33a7ac62825671d91dc2d0ae4146652fde7aab
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-26 20:13:59 -0700

    load DOM of div.rc before parsing inner content, fewer messages for logging

--- commands/general/question.js
+++ commands/general/question.js
@@ -128,29 +128,27 @@ function retrieveAnswerFromKnowledgePanel(message, cheerioDOM) {
  * @param cheerioDOM
  */
 function sendSearchResultsAsEmbeddedMessage(message, cheerioDOM) {
-    message.channel.send('Attempting to get data from search results');
-
     // Remove the "People also ask" section as these _aren't_ the thing we want an answer to
     cheerioDOM('div.g.kno-kp.mnr-c.g-blk').remove();
 
     let results = [];
 
-    cheerioDOM('div.rc').each(function () {
-        let link = cheerioDOM('div > a').attr('href');
-        message.channel.send(`Found link ${link}`);
-        let title = cheerioDOM('div > a > h3.LC20lb > span').text();
-        message.channel.send(`Link title ${title}`);
-        let description = cheerioDOM('div.IsZvec > div > span:not([class!=""])').text();
-        message.channel.send(`Link description ${description}`);
+    cheerioDOM('div.rc').some( (index, value) => {
+        const innerDOM = cheerio.load(value);
+
+        let link = innerDOM('div > a').attr('href');
+        let title = innerDOM('div > a > h3.LC20lb > span').text();
+        let description = innerDOM('div.IsZvec > div > span:not([class!=""])').text();
+        message.channel.send(`Link: ${link}, title: ${title}, desc: ${description}`)
 
         let embedMessage = new Discord.MessageEmbed()
             .setTitle(`[**${title}**](${link})`)
             .setDescription(description);
 
         results.push(embedMessage);
-    });
 
-    message.channel.send(`Attempting to send embedded messages ${results}`);
+        return results.length === 3;
+    });
 
     for (const result of results) {
         message.channel.send(result);
