0d49203a0f1679b859185951ba68d0868bc5b1b8
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-27 19:03:46 -0700

    stopwatch init

---
+++ commands/general/stopwatch.js
@@ -0,0 +1,26 @@
+const prettyMilliseconds = require('pretty-ms');
+
+const stopwatchMap = new Map();
+
+module.exports = {
+    name: 'stopwatch',
+    description: 'Use -stopwatch {name} to time something! Once to start, again to stop.',
+    execute(client, message, args) {
+
+        if (args.length < 1) {
+            message.channel.send('What are you trying to time? Include a name for your stopwatch!')
+        }
+
+        let name = args.join(' ');
+
+        if (stopwatchMap.has(name)) {
+            let startTime = stopwatchMap.get(name);
+            let endTime = new Date() - startTime;
+
+            message.channel.send(`${name} took ${prettyMilliseconds(endTime)}`);
+        } else {
+            stopwatchMap.set(name, new Date());
+            message.channel.send(`Started a timer for ${name}!`);
+        }
+    }
+}
