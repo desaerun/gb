95b9443f49614fcad02061ea27279dfe0e37b1fc
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-28 16:24:18 -0700

    undoing function param removal

--- commands/dev/print-env.js
+++ commands/dev/print-env.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "print-env",
     description: "prints environment variables",
-    execute: function (client,message) {
+    execute: function (client,message,args) {
         message.channel.send(`
             \`\`\`DISCORD_KEY=${process.env.BOT_TOKEN}\`\`\`
             \`\`\`YOUTUBE_KEY=${process.env.YOUTUBE_TOKEN}\`\`\`
--- commands/dev/throw-exception.js
+++ commands/dev/throw-exception.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "throw-exception",
     description: "Throws a test exception",
-    execute: function () {
+    execute: function (client,message,args) {
         throw new Error("This is a test error");
     }
 }
