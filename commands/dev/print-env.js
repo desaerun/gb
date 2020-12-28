08063ac09e1eb0a641c1b9c61bf6a06c10b4441a
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-28 16:30:07 -0700

    tesing unused param removal again since that isn't what broke

--- commands/dev/print-env.js
+++ commands/dev/print-env.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "print-env",
     description: "prints environment variables",
-    execute: function (client,message,args) {
+    execute: function (client,message) {
         message.channel.send(`
             \`\`\`DISCORD_KEY=${process.env.BOT_TOKEN}\`\`\`
             \`\`\`YOUTUBE_KEY=${process.env.YOUTUBE_TOKEN}\`\`\`
--- commands/dev/print-log.js
+++ commands/dev/print-log.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports = {
     name: "print-log",
     description: "prints the logfile",
-    execute: async function (client, message, args) {
+    execute: async function (client, message) {
         message.channel.send(`Contents of bot log file: \`\`\`${await readLog('/var/log/groidbot.log')}\`\`\``);
         message.channel.send(`Contents of pm2 status log file: \`\`\`${await readLog('/home/groidbot/.pm2/pm2.log')}\`\`\``);
         message.channel.send(`Contents of pm2 stdout log file: \`\`\`${await readLog('/home/groidbot/.pm2/logs/groidbot-out.log')}\`\`\``);
