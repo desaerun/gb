776cb7dc145c22114726e175dd8d5683fe20c549
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-29 01:20:52 -0700

    changing formatting in help message

--- commands/general/help.js
+++ commands/general/help.js
@@ -2,8 +2,8 @@ const name = 'help';
 const description = 'Prints a message telling users how to get a list of commands or help on a specific command.'
 
 async function execute(client, message) {
-    message.channel.send('Welcome, groid! To get started, type _-commands_ for a list of commands, or ' +
-                              'type _-{commandName} help_ to get more information about a certain command.');
+    message.channel.send('Welcome, groid! To get started, type `-commands` for a list of commands, or ' +
+                              'type `-{commandName} help` to get more information about a certain command.');
 }
 
 module.exports = {
