9f1618f73d987980341214a95aaac2006b85b6bf
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-29 20:24:39 -0700

    utilize existing function for snowflake->timestamp conversion

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,14 +1,12 @@
+const snowflakeToTimestamp = require(".../tools/snowflakeToTimestamp");
+
 module.exports = {
     name: 'ping',
     description: "this is a ping command",
     execute(client, message) {
-        let msgTimestamp = snowflakeToUnixMs(message.id);
+        let msgTimestamp = snowflakeToTimestamp(message.id);
         let latency = msgTimestamp - Date.now();
 
         message.channel.send(`pong (${latency}ms)`);
     }
-}
-
-function snowflakeToUnixMs(snowflake) {
-    return Math.floor((snowflake/4194304)+1420070400000);
 }
