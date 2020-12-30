474acd1b2f4feae6f4ee963585500e0618c2031f
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-29 20:17:38 -0700

    ping ms

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -2,6 +2,13 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command",
     execute(client, message) {
-        message.channel.send('pong');
+        let msgTimestamp = snowflakeToUnixMs(message.id);
+        let latency = msgTimestamp - Date.now();
+
+        message.channel.send(`pong (${latency}ms)`);
     }
+}
+
+function snowflakeToUnixMs(snowflake) {
+    return Math.floor((snowflake/4194304)+1420070400000);
 }
