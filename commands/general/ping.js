e364c1b21a2ca4fface23bf6c54e2f597b87aae8
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-29 20:29:58 -0700

    directory fix

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,4 +1,4 @@
-const snowflakeToTimestamp = require(".../tools/snowflakeToTimestamp");
+const snowflakeToTimestamp = require("../../tools/snowflakeToTimestamp");
 
 module.exports = {
     name: 'ping',
