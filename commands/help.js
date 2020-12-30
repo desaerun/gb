7361440e040e6555bfa60ae9afba7f093f46d923
Alex Jarvis <desaerun@gmail.com>
2020-12-30 00:05:57 -0500

    push everything inside module.exports

--- commands/help.js
+++ commands/help.js
@@ -3,73 +3,73 @@ const CONFIG = require('../config/config');
 
 const fs = require('fs');
 
-const name = 'help';
-const description = 'Prints a message telling users how to get a list of commands or help on a specific command.'
-const args = [
-    {
-        param: 'commandName',
-        type: 'string',
-        description: 'A string representing the name of the command you need help with',
-        required: false,
-    }
-];
-
-async function execute(client, message, args) {
-    if (args.length === 0) {
-        let response = 'List of Commands:';
-
-        function indent(level) {
-            let indent_string = '';
-            for (var i = 0; i < level; i++) {
-                indent_string += '    ';
-            }
-            return indent_string;
+module.exports = {
+    name: 'help',
+    description: 'Prints a message telling users how to get a list of commands or help on a specific command.',
+    args: [
+        {
+            param: 'commandName',
+            type: 'string',
+            description: 'A string representing the name of the command you need help with',
+            required: false,
         }
+    ],
+    async execute(client, message, args) {
+        if (args.length === 0) {
+            let response = 'List of Commands:';
 
-        function listCommands(subdir_name = "", level = 0) {
-            const full_current_dir = `./commands/${subdir_name}`;
-            const command_files = fs.readdirSync(full_current_dir);
-            if (CONFIG.verbosity >= 2) {
-                console.log(`Directory listing: ${command_files}`);
+            function indent(level) {
+                let indent_string = '';
+                for (var i = 0; i < level; i++) {
+                    indent_string += '    ';
+                }
+                return indent_string;
             }
 
-            for (const file_name of command_files) {
+            function listCommands(subdir_name = "", level = 0) {
+                const full_current_dir = `./commands/${subdir_name}`;
+                const command_files = fs.readdirSync(full_current_dir);
                 if (CONFIG.verbosity >= 2) {
-                    console.log(`full current dir: ${full_current_dir}`);
-                    console.log(`current subdir: ${subdir_name}`);
+                    console.log(`Directory listing: ${command_files}`);
                 }
-                if (file_name.endsWith('.js')) {
+
+                for (const file_name of command_files) {
                     if (CONFIG.verbosity >= 2) {
-                        console.log(`loading file:  ./${subdir_name}/${file_name}`);
+                        console.log(`full current dir: ${full_current_dir}`);
+                        console.log(`current subdir: ${subdir_name}`);
                     }
-                    if (file_name === 'help.js') {
-                        response += `\n${indent(level)}${CONFIG.prefix}_${this.name}_: ${this.description}`;
-                        continue;
+                    if (file_name.endsWith('.js')) {
+                        if (CONFIG.verbosity >= 2) {
+                            console.log(`loading file:  ./${subdir_name}/${file_name}`);
+                        }
+                        if (file_name === 'help.js') {
+                            response += `\n${indent(level)}${CONFIG.prefix}_${this.name}_: ${this.description}`;
+                            continue;
+                        }
+                        const command = require(`./${subdir_name}/${file_name}`);
+                        response += `\n${indent(level)}${CONFIG.prefix}_${command.name}_: ${command.description}`
+                    } else if (fs.statSync(`${full_current_dir}/${file_name}`).isDirectory()) {
+                        if (CONFIG.verbosity >= 2) {
+                            console.log(`${indent(level)}Recursing into directory ${full_current_dir}${file_name}`);
+                        }
+                        response += (`\n${indent(level)}${file_name} commands:`).replace("_", " ");
+                        listCommands(`${subdir_name}${file_name}`, level + 1);
                     }
-                    const command = require(`./${subdir_name}/${file_name}`);
-                    response += `\n${indent(level)}${CONFIG.prefix}_${command.name}_: ${command.description}`
-                } else if (fs.statSync(`${full_current_dir}/${file_name}`).isDirectory()) {
-                    if (CONFIG.verbosity >= 2) {
-                        console.log(`${indent(level)}Recursing into directory ${full_current_dir}${file_name}`);
-                    }
-                    response += (`\n${indent(level)}${file_name} commands:`).replace("_", " ");
-                    listCommands(`${subdir_name}${file_name}`, level + 1);
                 }
+                return response;
             }
-            return response;
+
+            message.channel.send(listCommands());
         }
 
-        message.channel.send(listCommands());
-    }
+        const helpWithCommand = args[0];
 
-    const helpWithCommand = args[0];
-
-    if (client.commands.has(helpWithCommand)) {
-        const embedMessage = getHelpMessage(client.commands.get(helpWithCommand));
-        await message.channel.send(embedMessage);
+        if (client.commands.has(helpWithCommand)) {
+            const embedMessage = getHelpMessage(client.commands.get(helpWithCommand));
+            await message.channel.send(embedMessage);
+        }
     }
 }
-
 /**
  * Constructs a MessageEmbed object from member fields of the command,
  * including any custom helpText in order to relay helpful information about the
@@ -80,10 +80,10 @@ async function execute(client, message, args) {
  */
 function getHelpMessage(command) {
     let fields = [];
-    fields[0] = {
+    fields.push({
         name: 'Description',
         value: command.description
-    };
+    });
     let fullCommand = `${CONFIG.prefix}${command.name}`;
     if (command.args) {
         for (const currentArg of command.args) {
@@ -126,11 +126,4 @@ function getHelpMessage(command) {
         .setTitle(`**${command.name}**`)
         .setDescription(`\`${fullCommand}\``)
         .addFields(fields);
-}
-
-module.exports = {
-    name: name,
-    description: description,
-    args: args,
-    execute: execute
 }
