3f28974155ed61d30f9984dcaff62071e8a70161
Charles <Charles.Cheney@retailerwebservices.com>
2020-12-29 20:41:40 -0700

    testing client.ws.ping

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,16 +1,7 @@
-const snowflakeToTimestamp = require("../../tools/snowflakeToTimestamp");
-
 module.exports = {
     name: 'ping',
     description: "this is a ping command",
     execute(client, message) {
-        const now = Date.now();
-        const msgTimestamp = snowflakeToTimestamp(message.id);
-        const latency = now - msgTimestamp;
-
-        console.log(`now: ${now}`);
-        console.log(`msgTimestamp: ${msgTimestamp}`);
-
-        message.channel.send(`pong (${latency}ms)`);
+        message.channel.send(`pong (${client.ws.ping}ms)`);
     }
 }
