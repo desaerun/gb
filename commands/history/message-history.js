4f61127b9608a279bb616a293fe4855fd44b17d1
Alex Jarvis <desaerun@gmail.com>
2021-01-01 18:21:44 -0500

    make i a var

--- commands/history/message-history.js
+++ commands/history/message-history.js
@@ -72,7 +72,7 @@ async function execute(client, message, args) {
             }
         } else {
             //in case there are more edits than can fit in the MessageEmbed (it only supports 10 fields total)
-            let i;
+            var i;
             for (i = 0; (currentMessage.deleted && i < 6) || (!currentMessage.deleted && i < 7); i++) {
                 let formattedDatetime = moment(messageHistory[i].editTimestamp).format("MMM Do YYYY h:mm:ssa");
                 embedMessage.addField(`Edit on ${formattedDatetime}`, messageHistory[i].newContent);
