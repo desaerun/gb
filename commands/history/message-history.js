56b409e6e9efe5fece37069ada2c7f4f4d839a0e
Alex Jarvis <desaerun@gmail.com>
2021-01-03 07:51:49 -0500

    make dateFormat a const containing the format for date print outs

--- commands/history/message-history.js
+++ commands/history/message-history.js
@@ -46,27 +46,27 @@ async function execute(client, message, args) {
         message.channel.send("That message ID does not exist.");
         return false;
     }
-
+    const dateFormat = "dddd, MMMM Do YYYY @ hh:mm:ss a";
     const embedMessage = new Discord.MessageEmbed()
         .setTitle(`Message History for ${messageID}`)
         .setURL(`https://discord.com/channels/${currentMessage.guild}/${currentMessage.channel}/${messageID}`)
         .setDescription(`Posted by: ${currentMessage.author_displayName}`);
     let originalContent = currentMessage.content;
     if (currentMessage.deleted) {
-        embedMessage.addField(":x: Deleted:", moment(currentMessage.deleted).format("dddd, MMMM Do YYYY @ hh:mm:ss a"));
+        embedMessage.addField(":x: Deleted:", moment(currentMessage.deleted).format(dateFormat));
     }
     if (messageHistory.length > 0) { // if the message has an edit history
         console.log(`messageHistory: ${JSON.stringify(messageHistory)}`);
         originalContent = messageHistory[messageHistory.length - 1].oldContent;
         const mostRecentEdit = messageHistory.shift();
-        embedMessage.addField(`Current Content (edited on ${moment(mostRecentEdit.editTimestamp).format("MMM Do YYYY h:mm:ssa")})`, mostRecentEdit.newContent);
+        embedMessage.addField(`Current Content (edited on ${moment(mostRecentEdit.editTimestamp).format(dateFormat)})`, mostRecentEdit.newContent);
         if ((currentMessage.deleted && messageHistory.length <= 8) || (!currentMessage.deleted && messageHistory.length <= 9)) {
             logMessage(`There is edit history but it will fit in one Embed: ${messageHistory.length}`);
             for (const edit of messageHistory) {
-                let formattedDatetime = moment(edit.editTimestamp).format("MMM Do YYYY h:mm:ssa");
+                let formattedDatetime = moment(edit.editTimestamp).format(dateFormat);
                 embedMessage.addField(`Edit on ${formattedDatetime}`, edit.newContent);
             }
-            embedMessage.addField(`Original Content (posted ${moment(currentMessage.timestamp).format("MMM Do YYYY h:mm:ssa")})`, originalContent);
+            embedMessage.addField(`Original Content (posted ${moment(currentMessage.timestamp).format(dateFormat)})`, originalContent);
             try {
                 await message.channel.send(embedMessage);
             } catch (e) {
@@ -77,7 +77,7 @@ async function execute(client, message, args) {
             logMessage(`There are more edits than can fit in one MessageEmbed: ${messageHistory.length}`);
             for (var currentMessagePointer = 0; (currentMessage.deleted && currentMessagePointer < 7) || (!currentMessage.deleted && currentMessagePointer < 8); currentMessagePointer++) {
                 logMessage(`currentMessagePointer: ${currentMessagePointer}`);
-                let formattedDatetime = moment(messageHistory[currentMessagePointer].editTimestamp).format("MMM Do YYYY h:mm:ssa");
+                let formattedDatetime = moment(messageHistory[currentMessagePointer].editTimestamp).format(dateFormat);
                 embedMessage.addField(`Edit on ${formattedDatetime}`, messageHistory[currentMessagePointer].newContent);
             }
             try {
@@ -90,18 +90,16 @@ async function execute(client, message, args) {
             for (; currentMessagePointer < messageHistory.length - 1;currentMessagePointer+=internalMessagePointer) {
                 logMessage(`currentMessagePointer: ${currentMessagePointer} | internalMessagePointer: ${internalMessagePointer}`);
                 const furtherEdits = new Discord.MessageEmbed()
-                    .setURL(`https://discord.com/channels/${currentMessage.guild}/${currentMessage.channel}/${messageID}`)
+                    .setURL(`https://discord.com/channels/${currentMessage.guild}/${currentMessage.channel}/${messageID}`);
                 for (var internalMessagePointer = 0;internalMessagePointer < 9 && messageHistory.length - 1 - (currentMessagePointer + internalMessagePointer) >= 0; internalMessagePointer++) {
                     let pointer = currentMessagePointer + internalMessagePointer;
-                    logMessage(`pointer: ${pointer} | currentMessagePointer: ${currentMessagePointer} | internalMessagePointer: ${internalMessagePointer}`)
-                    let formattedDatetime = moment(messageHistory[pointer].editTimestamp).format("MMM Do YYYY h:mm:ssa");
+                    let formattedDatetime = moment(messageHistory[pointer].editTimestamp).format(dateFormat);
                     furtherEdits.addField(`Edit on ${formattedDatetime}`, messageHistory[pointer].newContent);
                 }
                 if (messageHistory.length - (currentMessagePointer + internalMessagePointer) === 0) {
-                    furtherEdits.addField(`Original Content (posted ${moment(currentMessage.timestamp).format("MMM Do YYYY h:mm:ssa")})`, originalContent);
+                    furtherEdits.addField(`Original Content (posted ${moment(currentMessage.timestamp).format(dateFormat)}):`, originalContent);
                 }
                 try {
-                    logMessage(`Attempting to send further edit embed`);
                     await message.channel.send(furtherEdits);
                 } catch (e) {
                     throw e;
@@ -109,7 +107,7 @@ async function execute(client, message, args) {
             }
         }
     } else {
-        embedMessage.addField(`Original Content (posted ${moment(currentMessage.timestamp).format("MMM Do YYYY h:mm:ssa")})`, originalContent);
+        embedMessage.addField(`Original Content (posted ${moment(currentMessage.timestamp).format(dateFormat)}):`, originalContent);
         try {
             await message.channel.send(embedMessage);
         } catch (e) {
