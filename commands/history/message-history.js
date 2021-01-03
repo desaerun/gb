30fb9e0eb3b8d2658dab774220e1807fc4136abb
Alex Jarvis <desaerun@gmail.com>
2021-01-03 07:54:18 -0500

    formatting

--- commands/history/message-history.js
+++ commands/history/message-history.js
@@ -50,7 +50,7 @@ async function execute(client, message, args) {
     const embedMessage = new Discord.MessageEmbed()
         .setTitle(`Message History for ${messageID}`)
         .setURL(`https://discord.com/channels/${currentMessage.guild}/${currentMessage.channel}/${messageID}`)
-        .setDescription(`Posted by: ${currentMessage.author_displayName}`);
+        .setDescription(`Posted by: **${currentMessage.author_displayName}**`);
     let originalContent = currentMessage.content;
     if (currentMessage.deleted) {
         embedMessage.addField(":x: Deleted:", moment(currentMessage.deleted).format(dateFormat));
@@ -59,14 +59,13 @@ async function execute(client, message, args) {
         console.log(`messageHistory: ${JSON.stringify(messageHistory)}`);
         originalContent = messageHistory[messageHistory.length - 1].oldContent;
         const mostRecentEdit = messageHistory.shift();
-        embedMessage.addField(`Current Content (edited on ${moment(mostRecentEdit.editTimestamp).format(dateFormat)})`, mostRecentEdit.newContent);
+        embedMessage.addField(`Current Content (edited on ${moment(mostRecentEdit.editTimestamp).format(dateFormat)}):`, mostRecentEdit.newContent);
         if ((currentMessage.deleted && messageHistory.length <= 8) || (!currentMessage.deleted && messageHistory.length <= 9)) {
-            logMessage(`There is edit history but it will fit in one Embed: ${messageHistory.length}`);
             for (const edit of messageHistory) {
                 let formattedDatetime = moment(edit.editTimestamp).format(dateFormat);
                 embedMessage.addField(`Edit on ${formattedDatetime}`, edit.newContent);
             }
-            embedMessage.addField(`Original Content (posted ${moment(currentMessage.timestamp).format(dateFormat)})`, originalContent);
+            embedMessage.addField(`Original Content (posted ${moment(currentMessage.timestamp).format(dateFormat)}):`, originalContent);
             try {
                 await message.channel.send(embedMessage);
             } catch (e) {
@@ -74,21 +73,17 @@ async function execute(client, message, args) {
             }
         } else {
             //in case there are more edits than can fit in the MessageEmbed (it only supports 10 fields total)
-            logMessage(`There are more edits than can fit in one MessageEmbed: ${messageHistory.length}`);
             for (var currentMessagePointer = 0; (currentMessage.deleted && currentMessagePointer < 7) || (!currentMessage.deleted && currentMessagePointer < 8); currentMessagePointer++) {
-                logMessage(`currentMessagePointer: ${currentMessagePointer}`);
                 let formattedDatetime = moment(messageHistory[currentMessagePointer].editTimestamp).format(dateFormat);
                 embedMessage.addField(`Edit on ${formattedDatetime}`, messageHistory[currentMessagePointer].newContent);
             }
             try {
-                logMessage(`Sending first message`);
                 await message.channel.send(embedMessage);
             } catch (e) {
                 throw e;
             }
             //loop through more edits until end of edit history is reached
             for (; currentMessagePointer < messageHistory.length - 1;currentMessagePointer+=internalMessagePointer) {
-                logMessage(`currentMessagePointer: ${currentMessagePointer} | internalMessagePointer: ${internalMessagePointer}`);
                 const furtherEdits = new Discord.MessageEmbed()
                     .setURL(`https://discord.com/channels/${currentMessage.guild}/${currentMessage.channel}/${messageID}`);
                 for (var internalMessagePointer = 0;internalMessagePointer < 9 && messageHistory.length - 1 - (currentMessagePointer + internalMessagePointer) >= 0; internalMessagePointer++) {
