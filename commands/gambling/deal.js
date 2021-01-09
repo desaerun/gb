5d7fd6a40559ca3782606079562b591df1cbde78
Alex Jarvis <desaerun@gmail.com>
2021-01-09 13:41:42 -0500

    deal.js

---
+++ commands/gambling/deal.js
@@ -0,0 +1,68 @@
+//imports
+
+//module settings
+const name = "deal";
+const description = "deals a hand of cards";
+const params = [
+    {
+        param: 'size',
+        type: 'Integer',
+        description: 'How many cards to deal',
+        default: 1,
+        required: true,
+    },
+];
+//main
+async function execute(client, message, args) {
+    //build a deck
+    const suits = [
+        "Hearts",
+        "Spades",
+        "Diamonds",
+        "Clubs",
+    ];
+    const ranks = [
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+        "Ten",
+        "Jack",
+        "Queen",
+        "King",
+        "Ace",
+    ]
+    let cards = [];
+    let i = 0;
+    for (let suit=0;suit<=suits.length;suit++) {
+        for (let rank=0;rank<=ranks.length;rank++,i++) {
+            cards.push(`${ranks[rank]} of ${suits[suit]}`);
+        }
+    }
+
+    let hand = [];
+    for (i=1;i<=args[0];i++) {
+        let card = 1 + (Math.random() * cards.length);
+        hand.push(cards.splice(card,1)[0]);
+    }
+    try {
+        await message.channel.send(`Dealt the following cards: **${hand.join("**, **")}**`);
+    } catch (e) {
+        throw e;
+    }
+}
+
+//module export
+module.exports = {
+    name: name,
+    description: description,
+    params: params,
+    execute: execute,
+}
+
+//helper functions
