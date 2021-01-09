31ba3338ba5aa9b5b2816864d68cfec4d09c4124
Alex Jarvis <desaerun@gmail.com>
2021-01-09 14:10:18 -0500

    an integer must be passed in

--- commands/gambling/deal.js
+++ commands/gambling/deal.js
@@ -14,7 +14,7 @@ const params = [
 ];
 //main
 async function execute(client, message, args) {
-    if (isNaN(args[0]) || (parseFloat(args[0]) | 0) !== args[0]) {
+    if (isNaN(parseInt(args[0]))) {
         try {
             await message.channel.send("You must provide a valid integer input.");
         } catch (e) {
