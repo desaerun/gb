beff167c348dc93a0bd673e93e6e9e496d881299
Alex Jarvis <desaerun@gmail.com>
2021-01-10 16:50:41 -0500

    modify sendLongMessage.js to not break a word up between messages

--- tools/sendLongMessage.js
+++ tools/sendLongMessage.js
@@ -1,19 +1,19 @@
 async function sendLongMessage(text,channel,chunkSize = 2000) {
     const words = text.split(" ");
     let chunkWords = [];
-    let chunkLength = 0;
     for (let i=0;i<words.length;i++) {
+        let chunkLength = chunkWords.join(" ").length;
         if (chunkLength + words[i].length >= chunkSize) {
             try {
                 const msgChunk = chunkWords.join(" ");
                 await channel.send(msgChunk);
+                chunkWords = [];
             } catch (e) {
                 throw e;
             }
             i--;
         } else {
             chunkWords.push(words[i]);
-            chunkLength += words[i].length+1;
         }
     }
 }
