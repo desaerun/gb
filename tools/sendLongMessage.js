f7dbb9c6792f6d06fbe53d6c479da95ff76ed080
Alex Jarvis <desaerun@gmail.com>
2021-01-10 16:57:52 -0500

    console.log stuff

--- tools/sendLongMessage.js
+++ tools/sendLongMessage.js
@@ -2,9 +2,9 @@ async function sendLongMessage(text,channel,chunkSize = 2000) {
     const words = text.split(" ");
     let chunkWords = [];
     for (let i=0;i<words.length;i++) {
-        console.log(`${i}: ${words[i]}`);
+        console.log(`${i}: ${words[i]} | words.length: ${words.length} | chunkWords.length: ${chunkWords.length}`);
         let chunkLength = chunkWords.join(" ").length;
-        if (chunkLength + words[i].length >= chunkSize || (i === words.length-1 &&  chunkWords.length > 0)) {
+        if (chunkLength + words[i].length >= chunkSize || (i === words.length-1 && chunkWords.length > 0)) {
             try {
                 const msgChunk = chunkWords.join(" ");
                 await channel.send(msgChunk);
