29a97f24665787e980be6bbb49df0be1e56abe2d
Alex Jarvis <desaerun@gmail.com>
2021-01-12 19:43:48 -0500

    add getRand to utils.js

--- tools/utils.js
+++ tools/utils.js
@@ -7,3 +7,13 @@
 exports.getRandomArrayMember = function getRandomArrayMember(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
+
+/**
+ * Picks a random number between min and max (inclusive)
+ * @param min
+ * @param max
+ * @returns {number}
+ */
+exports.getRand = function getRand(min,max) {
+    return Math.floor(min + (Math.random() * (max-min)));
+}
