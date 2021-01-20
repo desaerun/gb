5cfc00020d2b1c0fb9a12427998b99fc711cb81a
Alex Jarvis <desaerun@gmail.com>
2021-01-20 03:00:52 -0500

    log in test.js why its not working?

--- listeners/test.js
+++ listeners/test.js
@@ -2,6 +2,7 @@ const name = "test";
 const description = "test listener";
 
 async function listen(client, message) {
+    console.log("made it to test.js");
     if (message.content.match(/^[\s]*\btest\b[\s]*$/i)) {
         await message.channel.send(`this is an test listener response`);
         return true;
