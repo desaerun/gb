0c3b7325212c1c5d461c80bf4f60ad9a2b7896ad
Alex Jarvis <desaerun@gmail.com>
2021-02-09 01:37:45 -0500

    switching to using coingecko

--- commands/general/crypto-price.js
+++ commands/general/crypto-price.js
@@ -1,5 +1,7 @@
 //imports
 const axios = require("axios");
+const fs = require("fs");
+const {createCanvas} = require("canvas");
 
 //module settings
 const name = "crypto-price";
@@ -21,31 +23,34 @@ async function execute(client, message, args) {
     }
 
     if (args.length > 1) {
-        await message.channel.send(`You cannot make multiple requests at once. (Requested ${args})`);
+        await message.channel.send(`You cannot make multiple requests at once. (Requested ${args.join(" ")})`);
         return;
     }
 
     let crypto = args[0].toUpperCase();
 
     try {
-        const response = await axios.get(`https://api.pro.coinbase.com/products/${crypto}-USD/stats`);
-        if (response.status === 200) {
-            let coinbaseData = response.data;
-
-            let priceDiff = coinbaseData.last - coinbaseData.open;
-            let percDiff = priceDiff / coinbaseData.open;
-
-            let curPriceFormatted = currencyFormat.format(coinbaseData.last);
-            let priceDiffFormatted = (priceDiff < 0 ? "" : "+") + currencyFormat.format(priceDiff);
-            let percDiffFormatted = (priceDiff < 0 ? "" : "+") + percentFormat.format(percDiff);
-
-            await message.channel.send(`1 ${crypto} = **${curPriceFormatted}** (**${priceDiffFormatted}**[**${percDiffFormatted}**] last 24hrs)`);
-        } else {
-            throw new Error(`Request returned status code ${response.status}`);
-        }
+        const coinsList = await getCoinsList();
+        const coinId = getCoinId(crypto,coinsList);
+        const price = await getCoinPrice(coinId);
+        const priceFormatted = currencyFormat.format(price);
+        await message.channel.send(`Coin price: ${priceFormatted}`);
     } catch (err) {
         await message.channel.send(`error fetching crypto price: ${err}`);
     }
+}
+async function drawCandles(ticker,range) {
+    let data = await getCoinOhlcData();
+
+    let width = 400;
+    let height = 400;
+
+    const canvas = createCanvas(width,height);
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, width, height);
+
 }
 
 //module export
@@ -68,4 +73,90 @@ const percentFormat = new Intl.NumberFormat("en-US",
         style: "percent",
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
-    })
+    });
+function getCoinId(symbol, coinsList) {
+    const coin = coinsList.find(c => c.symbol = symbol).id;
+    if (coin) {
+        return coin.id;
+    } else {
+        throw new Error("The coin could not be found.");
+    }
+}
+async function getCoinsList() {
+    const cryptoCoinsListFile = "data/cryptoCoinsList.json";
+    let coinsListData;
+    let allowedAge = (24 * 60 * 60 * 1000); // new coin list cache is fresh if it's newer than 1 day
+
+    //check if the coins list is already cached
+    if (!fs.existsSync(cryptoCoinsListFile))  {
+        //if not, fetch from API and write to cache
+        try {
+            coinsListData = await getAPICoinsList();
+        } catch (e) {
+            throw new Error(e);
+        }
+        fs.writeFileSync(cryptoCoinsListFile,JSON.stringify(coinsListData));
+    } else {
+        //if the coins list is already cached,
+        //check its age
+        const modified = fs.statSync(cryptoCoinsListFile).mtime.getTime();
+        if (modified < +Date.now() - allowedAge) {
+            //if it's older than the allowed age, fetch data from API and update the cache.
+            try {
+                coinsListData = await getAPICoinsList();
+            } catch (e)  {
+                throw new Error(e);
+            }
+        } else {
+            //else just pull it from the cache
+            coinsListData = JSON.parse(fs.readFileSync(cryptoCoinsListFile,"utf8"));
+        }
+    }
+    return coinsListData;
+}
+async function getAPICoinsList() {
+    try {
+        const coinsListRequest = await axios.get("https://api.coingecko.com/api/v3/coins/list");
+        if (coinsListRequest.status === 200) {
+            return coinsListRequest.data;
+        } else {
+            throw new Error(`HTTP status was not 200: ${coinsListRequest.status}`);
+        }
+    } catch (e) {
+        throw new Error(`There was an unexpected error retrieving API coin list: ${e}`);
+    }
+}
+async function getCoinOhlcData(coinId,vsCurrency = "usd", days = 1) {
+    try {
+        const coinOhlcRequest = await axios.get(`https://api.coingeck.com/api/v3/coins/${coinId}/ohlc`,{
+            params: {
+                vs_currency: vsCurrency,
+                days: days,
+            }
+        });
+        if (coinOhlcRequest.status === 200) {
+            return coinOhlcRequest.data;
+        } else {
+            throw new Error(`HTTP status not 200: ${coinOhlcRequest.status}`);
+        }
+    } catch (e) {
+        throw new Error(`There was an unexpected error retrieving OHLC data from API: ${e}`);
+    }
+}
+async function getCoinPrice(coinId,vsCurrency = "usd") {
+    try {
+        const coinPriceRequest = await axios.get("https://api.coingecko.com/api/v3/simple/price",{
+            params: {
+                ids: coinId,
+                vs_currencies: vsCurrency,
+            }
+        });
+        if (coinPriceRequest.status === 200) {
+            return coinPriceRequest.data[coinId][vsCurrency];
+        } else {
+            throw new Error(`HTTP status was not 200: ${coinPriceRequest.status}`);
+        }
+    } catch (e) {
+        throw new Error(`There was an unexpected error retrieving price data: ${e}`);
+    }
+}
