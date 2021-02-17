710a68de89938c52c29ffe6f9a9fbc8bb6f55585
Alex Jarvis <desaerun@gmail.com>
2021-02-17 05:12:44 -0500

    don't use the uri-encoded query string to send to axios as it is double-encoding it

--- commands/general/question.js
+++ commands/general/question.js
@@ -33,7 +33,7 @@ async function execute(client, message, args) {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36",
                 },
                 params: {
-                    q: queryUriString,
+                    q: query,
                 }
             });
             if (response.status === 200) {
