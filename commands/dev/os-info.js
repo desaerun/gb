bf7c23ff0857ad815135bdc6df70d445ec054b62
Alex Jarvis <desaerun@gmail.com>
2021-02-22 13:22:02 -0500

    os-info command

---
+++ commands/dev/os-info.js
@@ -0,0 +1,70 @@
+//imports
+const os = require("os");
+const prettyMilliseconds = require("pretty-ms");
+const {sendLongMessage} = require("../../tools/utils");
+
+//module settings
+const name = "os-info";
+const description = "prints information about the OS the bot is running on"
+
+//main
+async function execute(client, message) {
+    let output;
+    output += `OS info:\n`;
+    output += ` Uptime: ${prettyMilliseconds(os.uptime * 1000)}`;
+
+    output += `  Hostname: ${os.hostname()}\n`;
+
+    //architecture info
+    const arch = os.arch();
+    const version = os.version();
+    output += `  Architecture: \`${arch} (${version})\`\n`;
+
+    output += `  Home path: \`${os.homedir()}\`\n`;
+
+    //cpu info
+    output += `  CPUs:\n`
+    const cpus = os.cpus();
+    for (let i = 0; i < cpus.length; i++) {
+        const cpu = cpus[i];
+        output += `${i}: ${cpu.model}\n`;
+    }
+
+
+
+
+    output += `  Memory: \`${os.freemem()}B/${os.totalmem()}B\`\n`;
+
+    output += `  Network interfaces:\n`;
+    const networkInterfaces = os.networkInterfaces();
+    const skipInternalBindings = false;
+
+    for (const [interf,bindings] of Object.entries(networkInterfaces)) {
+        //skip internal network interfaces
+        if (bindings.some(a => a.internal === true) && skipInternalBindings === true) {
+            continue;
+        }
+
+        output += `    Interface \`${interf}\`:\n`;
+        for (let i = 0; i < bindings.length; i++) {
+            output += `      Binding \`${i}\`:\n`;
+            output += `        Address: \`${bindings[i].address}\`\n`;
+            output += `        Netmask: \`${bindings[i].netmask}\`\n`;
+            output += `        Family: \`${bindings[i].family}\`\n`;
+            output += `        MAC Address: \`${bindings[i].mac}\`\n`;
+            output += `        Address: \`${bindings[i].address}\`\n`;
+            output += `        Internal: \`${bindings[i].internal}\`\n`;
+            output += `        CIDR: \`${bindings[i].cidr}\`\n`;
+        }
+    }
+    await sendLongMessage(output,message.channel);
+}
+
+//module export
+module.exports = {
+    name: name,
+    description: description,
+    execute: execute,
+}
+
+//helper functions
