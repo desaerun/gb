657f8140845e5e129a3fba5857b053b6ca7ca4dc
Alex Jarvis <desaerun@gmail.com>
2021-02-24 21:39:34 -0500

    adjust timestamp utcOffset calculation

--- commands/history/random-message.js
+++ commands/history/random-message.js
@@ -51,10 +51,11 @@ async function execute(client, message, args, forceGuildID = null, forceChannelI
     let utcOffset = moment(timestamp).utcOffset();
     let offsetMs = -utcOffset * 60 * 1000;
     console.log(`utcOffset: ${utcOffset}|offsetMs: ${offsetMs}`);
+    timestamp += offsetMs; // add the UTC offset
 
     //calculate midnight on both ends of the day provided
     timestamp -= timestamp % (24 * 60 * 60 * 1000); //subtract minutes since midnight
-    timestamp += offsetMs; // add the UTC offset
+
 
     let end_timestamp = timestamp + (24 * 60 * 60 * 1000) - 1; //get 11:59:59.999 at the end of that day
     console.log(`Selecting messages between (${timestamp})${moment(timestamp).format("MMMM Do YYYY HH:mm:ss a")} and (${end_timestamp})${moment(end_timestamp).format("MMMM Do YYYY HH:mm:ss a")}`);
@@ -96,7 +97,7 @@ async function execute(client, message, args, forceGuildID = null, forceChannelI
     const humanMessageResults = allMessages.filter(element => !element.author_isBot);
     let noHumanMessages = (humanMessageResults.length === 0);
     if (noHumanMessages) {
-        sendMessage(`There were no messages on ${moment(timestamp).format("dddd MMMM Do YYYY")}`, channel);
+        await sendMessage(`There were no messages on ${moment(timestamp).format("dddd MMMM Do YYYY")}`, channel);
         return false;
     } else {
         if (allMessages.length < 3) {
@@ -119,7 +120,6 @@ async function execute(client, message, args, forceGuildID = null, forceChannelI
             let last = randomHumanMessageIndex + 2;
             selectedMessages = allMessages.slice(first, last).reverse();
         }
-        console.log(`Selected messages: ${JSON.stringify(selectedMessages)}`);
 
         for (const messageRow of selectedMessages) {
             console.log(`Current message: ${JSON.stringify(messageRow)}`);
@@ -129,6 +129,7 @@ async function execute(client, message, args, forceGuildID = null, forceChannelI
             if (messageRow.content) {
                 embedMessage.addField("\u200b", messageRow.content)
             }
+            embedMessage.addField("\u200b","\u200b");
             embedMessage.addField(humanTimedate,`[**Jump to Message**](https://discord.com/channels/${messageRow.guild}/${messageRow.channel}/${messageRow.id})`);
             if (messageRow.attachmentURL) {
                 embedMessage.setImage(messageRow.attachmentURL);
