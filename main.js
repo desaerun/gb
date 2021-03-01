cf9130bfe1119bee6bb3f2b6b0c48c1916242e21
Alex Jarvis <desaerun@gmail.com>
2021-03-01 02:52:10 -0500

    fix direct message block

--- main.js
+++ main.js
@@ -265,7 +265,7 @@ async function incomingMessageHandler(message) {
         await captureMessage(client, message, true);
     } else if (message.channel.type === "dm") {
         sendMessage("We do not currently support bot commands via Direct Message.", message.channel);
-        return;
+        return true;
     }
 
     // Ignore my own messages
