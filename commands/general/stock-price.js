19eb49c6a794e40e68d6bc7c90a605be42c54ed1
Alex Jarvis <desaerun@gmail.com>
2021-03-09 10:00:20 -0500

    fix module export?

--- commands/general/stock-price.js
+++ commands/general/stock-price.js
@@ -50,7 +50,6 @@ const getTickerData = async function (ticker) {
         await sendMessage(`error fetching stock price: ${err}`, message.channel);
     }
 }
-exports.getTickerData = getTickerData;
 
 const currencyFormat = new Intl.NumberFormat("en-US",
     {
