353942be46a36ecee71c87562cd66abe48d69755
Alex Jarvis <desaerun@gmail.com>
2021-03-09 09:36:30 -0500

    add stock-price.js

---
+++ commands/general/stock-price.js
@@ -0,0 +1,67 @@
+//imports
+const axios = require("axios");
+const {sendMessage} = require("../../tools/sendMessage");
+
+//module settings
+const name = "stock-price";
+const description = "Retrieves stock prices from Finnhub API (in USD)";
+const params = [
+    {
+        param: "stockTicker",
+        type: "String",
+        description: "a stock ticker (GME, MOON)",
+        default: "GME",
+    }
+];
+
+//main
+const execute = async function (client, message, args) {
+    if (args.length > 1) {
+        await sendMessage(`You cannot make multiple requests at once. (Requested ${args})`, message.channel);
+        return;
+    }
+
+    let ticker = args[0].toUpperCase();
+    const token = "c13ockf48v6qin45qo40";
+    try {
+        const response = await axios.get(`https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${token}`);
+        if (response.status === 200) {
+            let tickerData = response.data;
+
+            let priceDiff = tickerData.c - tickerData.pc;
+            let percDiff = priceDiff / tickerData.o;
+
+            let curPriceFormatted = currencyFormat.format(tickerData.c);
+            let priceDiffFormatted = (priceDiff < 0 ? "" : "+") + currencyFormat.format(priceDiff);
+            let percDiffFormatted = (priceDiff < 0 ? "" : "+") + percentFormat.format(percDiff);
+
+            await sendMessage(`${ticker} = **${curPriceFormatted}** (**${priceDiffFormatted}**[**${percDiffFormatted}**] last 24hrs)`, message.channel);
+        } else {
+            throw new Error(`Request returned status code ${response.status}`);
+        }
+    } catch (err) {
+        await sendMessage(`error fetching stock price: ${err}`, message.channel);
+    }
+}
+
+//module export
+module.exports = {
+    name: name,
+    description: description,
+    params: params,
+    execute: execute,
+}
+
+//helper functions
+const currencyFormat = new Intl.NumberFormat("en-US",
+    {
+        style: "currency",
+        currency: "USD"
+    });
+
+const percentFormat = new Intl.NumberFormat("en-US",
+    {
+        style: "percent",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })
