be1a50a75ab784e61927f0246c90b763429a1594
Alex Jarvis <desaerun@gmail.com>
2021-05-25 04:27:31 -0400

    fixes/optimizations for sendMessage:  -sendMessage now supports attachments.  -sendMessage should properly suppress embeds modified some code in play.js to support these changes

--- tools/sendMessage.js
+++ tools/sendMessage.js
@@ -7,12 +7,13 @@ const {uwuifyIfUwuMode} = require("./uwuify");
  *
  * @param input
  * @param target -- the Discord.js Channel/User object to send to
+ * @param attachment -- a Discord.js MessageAttachment object to attach
  * @param suppressEmbeds -- whether or not auto-generated Embeds should be suppressed
  * @param forceNormalText -- forces disabling uwu-mode
  * @param chunkSize -- the maximum size of each message
  * @returns{Promise<"discord.js".Message>}
  */
-exports.sendMessage = async function (input, target, suppressEmbeds = false, forceNormalText = false, chunkSize = 2000) {
+exports.sendMessage = async function (input, target, attachment, suppressEmbeds = false, forceNormalText = false, chunkSize = 2000) {
     if (input instanceof Discord.MessageEmbed) {
         if (forceNormalText) {
             for (const field of input.fields) {
@@ -51,14 +52,9 @@ exports.sendMessage = async function (input, target, suppressEmbeds = false, for
         try {
             output = await target.send(input);
         } catch (e) {
-            output = await target.send(`Failed to send embedded message: ${e}`);
+            output = await target.send(`Failed to send embedded message: ${e.stack}`);
         }
         return output;
-    }
-    if (suppressEmbeds) {
-        const urlRegex = '((?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?)';
-        const url = new RegExp(urlRegex, "ig");
-        input = input.replace(url, "<$1>");
     }
     if (!forceNormalText) {
         input = uwuifyIfUwuMode(input, target.client.uwuMode);
@@ -68,24 +64,22 @@ exports.sendMessage = async function (input, target, suppressEmbeds = false, for
     let lastMessage;
     for (let i = 0; i < words.length; i++) {
         const msgChunk = chunkWords.join(" ");
-        if (msgChunk.length + words[i].length >= chunkSize) {
-            try {
+        //if the current chunk plus the current word would go over the chunk size,
+        //or if we're on the last word of the message, send the message
+        if (msgChunk.length + words[i].length >= chunkSize ||
+            i === words.length - 1) {
+            if (i === words.length - 1 && attachment) {
+                lastMessage = await target.send(msgChunk, attachment);
+            } else {
                 lastMessage = await target.send(msgChunk);
-            } catch (e) {
-                throw e;
+            }
+            if (suppressEmbeds) {
+                await lastMessage.suppressEmbeds(true);
             }
             chunkWords = [];
             i--;
         } else {
             chunkWords.push(words[i]);
-        }
-    }
-    if (chunkWords.length > 0) {
-        const msgChunk = chunkWords.join(" ");
-        try {
-            lastMessage = await target.send(msgChunk);
-        } catch (e) {
-            throw e;
         }
     }
     return lastMessage;
